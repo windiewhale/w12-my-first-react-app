d18f8b62fa6c6432fa102195b76929e528fa1848
windiewhale <feketebodza@gmail.com>
2022-04-07 21:02:06 +0200

    wtf

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/button/button';
+const text = "blablablablu"
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {text}
+        </p>
+        <p>so bjutiful, so wanderful</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/apptwo.js
@@ -0,0 +1,13 @@
+function AppTwo() {
+    return (
+        <>
+            <div></div>
+            <div className="AppTwo">
+                <p>surprise!</p>
+                <div></div>
+            </div>
+        </>
+    );
+}
+
+export default AppTwo
